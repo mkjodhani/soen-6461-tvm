29f690798b6e1c86c5b999191192172e49965063
Nithin Harikrishnan <nitin.h.nair@gmail.com>
2023-04-10 13:00:19 -0400

    AdminController implemented

---
+++ iGO/src/main/java/com/igo/controller/admin/OpusPriceController.java
@@ -0,0 +1,94 @@
+package com.igo.controller.admin;
+
+import com.igo.IGoApplication;
+import com.igo.models.data.Data;
+import com.igo.models.fares.Cost;
+import com.igo.models.localization.Language;
+import com.igo.models.person.Customer;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * @author mkjodhani
+ * @project
+ * @since 22/03/23
+ */
+public class OpusPriceController implements Observer {
+    public Label opusPlanDetails;
+    public Label customerTypeLabel;
+    public Label opusTypeLabel;
+    public Label priceLabel;
+    public Button updateButton;
+    @FXML
+    ComboBox<Customer.TYPES> customerTypeComboBox;
+    @FXML
+    ComboBox<Cost.PERIOD> opusTypeComboBox;
+    @FXML
+    TextField price;
+    Button closeParentButton = new Button();
+    public void initialize(){
+        Language.getReference().addObserver(this);
+        opusPlanDetails.setText(Language.getReference().getLabel("opusPlanDetails"));
+        customerTypeLabel.setText(Language.getReference().getLabel("customerType"));
+        opusTypeLabel.setText(Language.getReference().getLabel("opusRechargeTypeLabel"));
+        priceLabel.setText(Language.getReference().getLabel("price"));
+        updateButton.setText(Language.getReference().getLabel("update"));
+        customerTypeComboBox.getItems().add(Customer.TYPES.CHILD);
+        customerTypeComboBox.getItems().add(Customer.TYPES.STUDENT);
+        customerTypeComboBox.getItems().add(Customer.TYPES.ADULT);
+        customerTypeComboBox.getItems().add(Customer.TYPES.SENIOR_CITIZEN);
+        opusTypeComboBox.getItems().add(Cost.PERIOD.ONE_WEEK);
+        opusTypeComboBox.getItems().add(Cost.PERIOD.ONE_MONTH);
+        opusTypeComboBox.getItems().add(Cost.PERIOD.FOUR_MONTH);
+    }
+
+    public void update(ActionEvent actionEvent) {
+        Customer.TYPES customerType = customerTypeComboBox.getValue();
+        Cost.PERIOD opusType = opusTypeComboBox.getValue();
+        if (customerType == null || opusType == null){
+            IGoApplication.showErrorDialogBox("Please provide valid information!","One of the field is empty.");
+            return;
+        }
+        else if (Cost.getOpusRechargeAmount(opusType,customerType) == -1){
+            IGoApplication.showErrorDialogBox("Please provide valid information!","This plan doesn't exist in the system.");
+            return;
+        }
+        double priceValue = Double.valueOf(price.getText());
+        Cost.updateOpusRechargeAmount(opusType,customerType,priceValue);
+        Data.getReference().notifyAllObservers();
+        IGoApplication.showDialogBox("Success!","Operation performed successfully!","Price has been updated!");
+        closeParentButton.fire();
+    }
+
+    public void fetch(ActionEvent actionEvent) {
+        Customer.TYPES customerType = customerTypeComboBox.getValue();
+        Cost.PERIOD opusType = opusTypeComboBox.getValue();
+        if (customerType == null || opusType == null){
+            return;
+        }
+        double priceValue =  Cost.getOpusRechargeAmount(opusType,customerType);
+        price.setText(String.valueOf(priceValue));
+    }
+    public Button getCloseParentButton() {
+        return closeParentButton;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        Platform.runLater(() ->{
+            opusPlanDetails.setText(Language.getReference().getLabel("opusPlanDetails"));
+            customerTypeLabel.setText(Language.getReference().getLabel("customerType"));
+            opusTypeLabel.setText(Language.getReference().getLabel("opusRechargeTypeLabel"));
+            priceLabel.setText(Language.getReference().getLabel("price"));
+            updateButton.setText(Language.getReference().getLabel("update"));
+        });
+    }
+}
--- iGO/src/main/java/com/igo/controller/admin/TicketPriceController.java
+++ iGO/src/main/java/com/igo/controller/admin/TicketPriceController.java
@@ -1,24 +1,37 @@
 package com.igo.controller.admin;
 
+import com.igo.IGoApplication;
 import com.igo.models.data.Data;
 import com.igo.models.fares.Cost;
-import com.igo.models.person.Customer;
+import com.igo.models.localization.Language;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.util.Observable;
+import java.util.Observer;
 
 /**
  * @author mkjodhani
  * @project
  * @since 23/03/23
  */
-public class TicketPrice {
+public class TicketPriceController implements Observer {
+    public Label ticketPriceDetailsLabel;
+    public Label ticketTypeLabel;
+    public Label priceLabel;
+    public Button updatePriceButton;
     @FXML
     ComboBox<Cost.PERIOD> ticketTypeComboBox;
     @FXML
     TextField price;
+    Button closeParentButton = new Button();
     public void initialize(){
+        Language.getReference().addObserver(this);
+        updateLabels();
         ticketTypeComboBox.getItems().add(Cost.PERIOD.ONE_TRIP);
         ticketTypeComboBox.getItems().add(Cost.PERIOD.TWO_TRIPS);
         ticketTypeComboBox.getItems().add(Cost.PERIOD.ONE_DAY);
@@ -29,11 +42,14 @@ public class TicketPrice {
     public void update(ActionEvent actionEvent) {
         Cost.PERIOD ticketType = ticketTypeComboBox.getValue();
         if (ticketType == null){
+            IGoApplication.showErrorDialogBox("Please provide valid information!","One of the field is empty.");
             return;
         }
         double priceValue = Double.valueOf(price.getText());
         Cost.updateTicketPriceByTimePeriod(ticketType,priceValue);
         Data.getReference().notifyAllObservers();
+        closeParentButton.fire();
+        IGoApplication.showDialogBox("Success!","Operation performed successfully!","Price has been updated!");
     }
     public void fetch(ActionEvent actionEvent) {
         Cost.PERIOD ticketType = ticketTypeComboBox.getValue();
@@ -42,5 +58,18 @@ public class TicketPrice {
         }
         double priceValue =  Cost.getTicketPriceByTimePeriod(ticketType);
         price.setText(String.valueOf(priceValue));
+    }
+    public Button getCloseParentButton() {
+        return closeParentButton;
+    }
+    private void updateLabels(){
+        ticketPriceDetailsLabel.setText(Language.getReference().getLabel("ticketPriceDetailsLabel"));
+        ticketTypeLabel.setText(Language.getReference().getLabel("ticketType"));
+        priceLabel.setText(Language.getReference().getLabel("price"));
+        updatePriceButton.setText(Language.getReference().getLabel("update"));
+    }
+    @Override
+    public void update(Observable o, Object arg) {
+        updateLabels();
     }
 }
